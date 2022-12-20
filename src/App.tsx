3e8e90074f66144d6c78e7018005026cbcac3ce4
Mate Nemeth <mate.nemeth@oander.hu>
2022-12-20 07:15:15 +0100

    feat: add portfolio article template

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Skills from './components/skills/Skills';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Nav/>
             <About/>
             <Skills/>
+            <Portfolio/>
             <Contact/>
             <Footer/>
         </>
--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import './portfolio.css';
 
+const data = [
+    {
+        title: 'Check out my repositories on github.com',
+        github: 'https://github.com/therealnemethmate?tab=repositories',
+    },
+];
+
 const Portfolio = () => {
     return (
-        <section id="portfolio">Portfolio</section>
+        <section id="portfolio">
+            <h5>My Recent Work</h5>
+            <h2>Portfolio</h2>
+            <div className="container portfolio__container">
+                <div>{/* TODO fill more portfolio material here */}</div>
+                {data.map(({ title, github }, id) => {
+                    return (
+                        <article className="portfolio__item" key={id}>
+                            <h3>{title}</h3>
+                            <div className="portfolio__item-cta">
+                                <a href={github} target="_blank" className="btn" rel="noreferrer">
+                                    Github
+                                </a>
+                            </div>
+                        </article>
+                    );
+                })}
+            </div>
+        </section>
     );
 };
 
